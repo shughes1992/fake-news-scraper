c83bc166951f647b9b517aca523fdcfd4e5c069d
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2018-06-18 21:22:51 -0400

    notes

--- models/note.js
+++ models/note.js
@@ -1,11 +1,11 @@
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 
-// var Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
-// var NoteSchema = new Schema({
-//   title: String,
-//   body: String
-// });
+var NoteSchema = new Schema({
+  title: String,
+  body: String
+});
 
-// // Export the Note model
-// module.exports = mongoose.model("Note", NoteSchema);
+// Export the Note model
+module.exports = mongoose.model("Note", NoteSchema);
